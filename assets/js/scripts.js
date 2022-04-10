619845cc81f799365e8d352033a49b1a1dbc3806
eliabedossantos <eliabe.s.o36@gmail.com>
2022-04-10 17:24:12 -0300

    first commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function(){
+
+    $("#sidebarCollapse").click(function() {
+        $(".sidebar").addClass("sidebar-active");
+        $(".sidebar").removeClass("sidebar-close");
+    });
+    $("#sidebarCollapse2").click(function() {
+        $(".sidebar").addClass("sidebar-active");
+        $(".sidebar").removeClass("sidebar-close");
+    });
+  
+    $("#sidebar-close").click(function() {
+        $(".sidebar").addClass("sidebar-close");
+        $(".sidebar").removeClass("sidebar-active");
+
+    });
+
+    $("#menu-mobile").click(function() {
+        $(".sidebar").toggleClass("sidebar-active");
+        $(".home").toggleClass("home--hidden");
+        $(".home-close").toggleClass("home-close--visible");
+
+    });
+});
